011deff710de04b338de6c65a17680a48d924efa
nestor <limnestor@consultant.com>
2024-05-12 13:40:24 +0800

    fix nirc, add sena; update sitemap

--- js/main.js
+++ js/main.js
@@ -290,6 +290,9 @@
                                         <a class="dropdown-item" href="owwa.html">Migrant Workers and Overseas Filipinos Act of 1995 (As Amended)</a>
                                     </li>
                                     <li>
+                                        <a class="dropdown-item" href="sena.html">Single Entry Approach (SEnA)</a>
+                                    </li>
+                                    <li>
                                         <a class="dropdown-item" href="irrowwa.html">Omnibus Rules And Regulations Implementing The Migrant Workers And Overseas Filipinos Act</a>
                                     </li>
                                     <li>
