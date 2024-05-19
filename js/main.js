c630f37ef7e6e02eadcc56d4f432de7203b4b096
nestor <limnestor@consultant.com>
2024-05-19 12:37:36 +0800

    add chattel mortgage law; updated sitemap

--- js/main.js
+++ js/main.js
@@ -342,6 +342,9 @@
                                         <a class="dropdown-item" href="bulksales.html">Bulk Sales Law</a>
                                     </li>
                                     <li>
+                                        <a class="dropdown-item" href="act1508.html">TheChattel Mortgage Law</a>
+                                    </li>
+                                    <li>
                                         <a class="dropdown-item" href="ra8791.html">General Banking Law</a>
                                     </li>
                                     <li>
