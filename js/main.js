50391da9ab3e4c025e4cbbb761b9a677ab125fc5
nestor <limnestor@consultant.com>
2024-10-14 22:44:13 +0800

    add google analytics tag

--- js/main.js
+++ js/main.js
@@ -605,6 +605,20 @@
             }
         }
     });
+
+    let gtag = `
+        <!-- Google tag (gtag.js) -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-319HK34R9S"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-319HK34R9S');
+            </script>
+    `;
+
+    $("head").prepend(gtag);
     
 })(jQuery);
 
