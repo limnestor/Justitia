49b2df769015d5eb1d6bbab32f9c30b447700437
nestor <limnestor@consultant.com>
2026-02-15 22:22:47 +0800

    add Foreign Currency Deposit Act of the Philippines; update sitemap; add note on bank secrecy law

--- js/main.js
+++ js/main.js
@@ -704,6 +704,9 @@
                                         <a class="dropdown-item icodalname" href="ra1405.html">Law on Secrecy of Bank Deposits</a>
                                     </li>
                                     <li>
+                                        <a class="dropdown-item icodalname" href="ra6426.html">Foreign Currency Deposit Act of the Philippines</a>
+                                    </li>
+                                    <li>
                                         <a class="dropdown-item icodalname" href="ra10607.html">The Insurance Code</a>
                                     </li>
                                     <li>
